a6d5951c8a948c9feee53da589ae5493aabbd858
cde-sous <cde-sous@student.42.fr>
2024-09-10 12:57:18 +0200

    Create bonus files and delete executable. TODO: Add here_doc management

---
+++ common-core/Circle2/pipex/pipex_cmds_bonus.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_cmds_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cde-sous <cde-sous@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/01 11:10:37 by cde-sous          #+#    #+#             */
+/*   Updated: 2024/09/10 12:55:01 by cde-sous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex_bonus.h"
+
+char	**copy_args(char **args)
+{
+	char	**copy;
+	int		i;
+
+	i = 0;
+	while (args[i])
+		++i;
+	copy = malloc(sizeof(char *) * (i + 1));
+	if (!copy)
+		return (NULL);
+	i = 0;
+	while (args[i])
+	{
+		copy[i] = ft_strdup(args[i]);
+		if (!copy[i])
+			return (NULL);
+		++i;
+	}
+	copy[i] = NULL;
+	return (copy);
+}
+
+t_cmd	*create_cmd(char *cmd_path, char **args)
+{
+	t_cmd	*cmd;
+
+	cmd = malloc(sizeof(t_cmd));
+	if (!cmd)
+		return (NULL);
+	cmd->name = cmd_path;
+	cmd->args = copy_args(args);
+	if (!cmd->args)
+		return (NULL);
+	cmd->next = NULL;
+	return (cmd);
+}
+
+void	cmd_add_back(t_cmd **head, t_cmd *new)
+{
+	t_cmd	*last;
+
+	if (!*head)
+	{
+		*head = new;
+		new->is_first = 1;
+		new->is_last = 1;
+		new->next = NULL;
+		return ;
+	}
+	last = *head;
+	while (last->next)
+		last = last->next;
+	last->next = new;
+	last->is_last = 0;
+	new->is_first = 0;
+	new->is_last = 1;
+	new->next = NULL;
+}
+
+t_cmd	*get_cmds(char **cmd, char **paths)
+{
+	t_cmd	*cmds;
+	t_cmd	*tmp;
+	char	**args;
+	char	**next;
+
+	cmds = NULL;
+	next = cmd;
+	if (!next)
+		return (NULL);
+	while (*next)
+	{
+		args = ft_split(*next, ' ');
+		if (!args)
+			return (NULL);
+		tmp = create_cmd(get_cmd_path(args[0], paths), args);
+		free_groups(args);
+		if (!tmp)
+			return (NULL);
+		cmd_add_back(&cmds, tmp);
+		++next;
+	}
+	return (cmds);
+}
