53a5a2e2b9b1a402dad68396516b04a7bb97c914
cde-sous <cde-sous@student.42.fr>
2024-09-27 15:58:08 +0200

    isnertion sort created

--- common-core/Circle2/push_swap/src/algorithms.c
+++ common-core/Circle2/push_swap/src/algorithms.c
@@ -6,7 +6,7 @@
 /*   By: cde-sous <cde-sous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 14:02:22 by cde-sous          #+#    #+#             */
-/*   Updated: 2024/09/27 15:17:44 by cde-sous         ###   ########.fr       */
+/*   Updated: 2024/09/27 15:56:25 by cde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,32 @@ void	algorithms(t_stacks *stacks)
 		ft_printf("use radix sort: %d\n", stacks->count);
 	else
 		ft_printf("use insertion sort: %d\n", stacks->count);
+}
+
+void	insertion_sort(t_stacks *stacks)
+{
+	t_list	*sorted;
+	t_list	*tmp;
+	t_list	*next;
+
+	sorted = NULL;
+	while (stacks->stack_a)
+	{
+		next = stacks->stack_a->next;
+		if (sorted == NULL || sorted->content >= stacks->stack_a->content)
+		{
+			stacks->stack_a->next = sorted;
+			sorted = stacks->stack_a;
+		}
+		else
+		{
+			tmp = sorted;
+			while (tmp->next && tmp->next->content < stacks->stack_a->content)
+				tmp = tmp->next;
+			stacks->stack_a->next = tmp->next;
+			tmp->next = stacks->stack_a;
+		}
+		stacks->stack_a = next;
+	}
+	stacks->stack_a = sorted;
 }
