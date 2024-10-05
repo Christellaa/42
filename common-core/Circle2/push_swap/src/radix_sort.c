578537280a061ca4011a652553c3301f16684cab
cde-sous <cde-sous@student.42.fr>
2024-10-05 11:31:41 +0200

    Fix get_bits_max to take a copy of nb_max value so it doesn't modfy the actual value in stack_a

--- common-core/Circle2/push_swap/src/radix_sort.c
+++ common-core/Circle2/push_swap/src/radix_sort.c
@@ -6,15 +6,45 @@
 /*   By: cde-sous <cde-sous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 09:55:20 by cde-sous          #+#    #+#             */
-/*   Updated: 2024/10/05 11:15:10 by cde-sous         ###   ########.fr       */
+/*   Updated: 2024/10/05 11:29:26 by cde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
+int	get_bits_max(t_stack *nb_max)
+{
+	int	bits_max;
+	int	value;
+
+	bits_max = 1;
+	value = *(int *)nb_max->content;
+	while (value)
+	{
+		value /= 2;
+		if (value == 0)
+			break ;
+		bits_max++;
+	}
+	return (bits_max);
+}
+
 void	radix_sort(t_stacks *stacks, t_stack *nb_max)
 {
+	int	bits_max;
+	int	stack_a_size;
+	int	i;
+
 	ft_printf("nbmax: %d: %d\n", *(int *)nb_max->content, nb_max->idx);
+	bits_max = get_bits_max(nb_max);
+	ft_printf("bits_max: %d\n", bits_max);
+	stack_a_size = ft_stacksize(stacks->stack_a);
+	i = 0;
+	while (i < stack_a_size)
+	{
+		ft_printf("stacka.content: %d\n", *(int *)stacks->stack_a->content);
+		stacks->stack_a = stacks->stack_a->next;
+		i++;
+	}
 	cleanup(stacks, NULL, 1);
-	//index_numbers(stacks);
 }
