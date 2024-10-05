d8adeec507be623434ee63c69955295e9ac1e257
cde-sous <cde-sous@student.42.fr>
2024-10-05 16:10:48 +0200

    Checking when to switch to insertion sorting

--- common-core/Circle2/push_swap/src/radix_sort.c
+++ common-core/Circle2/push_swap/src/radix_sort.c
@@ -6,11 +6,37 @@
 /*   By: cde-sous <cde-sous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 09:55:20 by cde-sous          #+#    #+#             */
-/*   Updated: 2024/10/05 15:32:29 by cde-sous         ###   ########.fr       */
+/*   Updated: 2024/10/05 16:09:50 by cde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
+
+int	is_mostly_sorted(t_stack *stack_a, int stack_a_size)
+{
+	t_stack	*tmp;
+	int		sorted_count;
+	int		current_idx;
+	int		ratio;
+
+	tmp = stack_a;
+	sorted_count = 0;
+	current_idx = 0;
+	while (tmp)
+	{
+		if (current_idx == tmp->idx)
+			sorted_count++;
+		tmp = tmp->next;
+		current_idx++;
+	}
+	ratio = (sorted_count * 100) / stack_a_size;
+	if (ratio >= 70)
+	{
+		//ft_printf("insertion sort");
+		return (1);
+	}
+	return (0);
+}
 
 int	get_bits_max(t_stack *nb_max)
 {
@@ -46,12 +72,11 @@ int	check_next_numbers_bit(t_stack *stack_a, int bit_pos)
 	return (1);
 }
 
-void	compare_bits(t_stacks *stacks, int bit_pos)
+void	compare_bits(t_stacks *stacks, int bit_pos, int stack_a_size)
 {
-	int	stack_a_size;
 	int	i;
+	int	stack_b_size;
 
-	stack_a_size = ft_stacksize(stacks->stack_a);
 	i = 0;
 	while (i < stack_a_size \
 		&& check_next_numbers_bit(stacks->stack_a, bit_pos))
@@ -62,24 +87,27 @@ void	compare_bits(t_stacks *stacks, int bit_pos)
 			ra(stacks);
 		i++;
 	}
+	stack_b_size = ft_stacksize(stacks->stack_b);
+	while (stack_b_size--)
+		pa(stacks);
 }
 
 void	radix_sort(t_stacks *stacks, t_stack *nb_max)
 {
 	int		bit_pos;
 	int		bits_max;
-	int		stack_b_size;
+	int		stack_a_size;
 
+	stack_a_size = ft_stacksize(stacks->stack_a);
 	bits_max = get_bits_max(nb_max);
 	bit_pos = 0;
 	while (bit_pos < bits_max)
 	{
 		if (check_sorted(stacks))
 			break ;
-		compare_bits(stacks, bit_pos);
-		stack_b_size = ft_stacksize(stacks->stack_b);
-		while (stack_b_size--)
-			pa(stacks);
+		compare_bits(stacks, bit_pos, stack_a_size);
+		is_mostly_sorted(stacks->stack_a, stack_a_size);
+			//insertion_sort(stacks);
 		bit_pos++;
 	}
 }
