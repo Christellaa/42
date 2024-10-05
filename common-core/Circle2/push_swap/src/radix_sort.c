093cd35006b05e25cd44ff798f4e03aadbb63c98
cde-sous <cde-sous@student.42.fr>
2024-10-05 22:01:45 +0200

    Delete useless functions, delete the function leading to inesrtion_sort -> optimization of radix sorting will be done instead

--- common-core/Circle2/push_swap/src/radix_sort.c
+++ common-core/Circle2/push_swap/src/radix_sort.c
@@ -6,37 +6,11 @@
 /*   By: cde-sous <cde-sous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 09:55:20 by cde-sous          #+#    #+#             */
-/*   Updated: 2024/10/05 16:09:50 by cde-sous         ###   ########.fr       */
+/*   Updated: 2024/10/05 22:00:21 by cde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
-
-int	is_mostly_sorted(t_stack *stack_a, int stack_a_size)
-{
-	t_stack	*tmp;
-	int		sorted_count;
-	int		current_idx;
-	int		ratio;
-
-	tmp = stack_a;
-	sorted_count = 0;
-	current_idx = 0;
-	while (tmp)
-	{
-		if (current_idx == tmp->idx)
-			sorted_count++;
-		tmp = tmp->next;
-		current_idx++;
-	}
-	ratio = (sorted_count * 100) / stack_a_size;
-	if (ratio >= 70)
-	{
-		//ft_printf("insertion sort");
-		return (1);
-	}
-	return (0);
-}
 
 int	get_bits_max(t_stack *nb_max)
 {
@@ -55,31 +29,13 @@ int	get_bits_max(t_stack *nb_max)
 	return (bits_max);
 }
 
-int	check_next_numbers_bit(t_stack *stack_a, int bit_pos)
-{
-	while ((*(int *)stack_a->content & (1 << bit_pos)) == 1)
-	{
-		stack_a = stack_a->next;
-		if (!stack_a)
-			return (0);
-	}
-	while ((*(int *)stack_a->content & (1 << bit_pos)) == 0)
-	{
-		stack_a = stack_a->next;
-		if (!stack_a || !stack_a->next->next)
-			return (0);
-	}
-	return (1);
-}
-
 void	compare_bits(t_stacks *stacks, int bit_pos, int stack_a_size)
 {
 	int	i;
 	int	stack_b_size;
 
 	i = 0;
-	while (i < stack_a_size \
-		&& check_next_numbers_bit(stacks->stack_a, bit_pos))
+	while (i < stack_a_size && !check_sorted(stacks))
 	{
 		if ((*(int *)stacks->stack_a->content & (1 << bit_pos)) == 0)
 			pb(stacks);
@@ -101,13 +57,10 @@ void	radix_sort(t_stacks *stacks, t_stack *nb_max)
 	stack_a_size = ft_stacksize(stacks->stack_a);
 	bits_max = get_bits_max(nb_max);
 	bit_pos = 0;
-	while (bit_pos < bits_max)
+	while (bit_pos < bits_max \
+		&& !check_sorted(stacks))
 	{
-		if (check_sorted(stacks))
-			break ;
 		compare_bits(stacks, bit_pos, stack_a_size);
-		is_mostly_sorted(stacks->stack_a, stack_a_size);
-			//insertion_sort(stacks);
 		bit_pos++;
 	}
 }
