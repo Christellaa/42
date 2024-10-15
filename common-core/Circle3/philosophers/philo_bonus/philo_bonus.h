23e8051645789d7078af56850d1c4762e80f0022
cde-sous <cde-sous@student.42.fr>
2024-10-15 17:15:30 +0200

    Create base for Philosophers project

---
+++ common-core/Circle3/philosophers/philo_bonus/philo_bonus.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cde-sous <cde-sous@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 17:09:00 by cde-sous          #+#    #+#             */
+/*   Updated: 2024/10/15 17:14:05 by cde-sous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
