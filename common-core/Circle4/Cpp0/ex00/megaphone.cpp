f18e12753e5e2f215817d2a83c4d83494120729b
Christella De Sousa <cde-sous@paul-f4Ar3s1.clusters.42paris.fr>
2025-02-14 09:43:26 +0100

    cpp00 ex00 done

---
+++ common-core/Circle4/Cpp0/ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+// #include <cstring>
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+int	main(int ac, char **av)
+{
+	size_t	i;
+
+	if (ac < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	std::string sentence;
+	i = 1;
+	while (av[i])
+		sentence += av[i++];
+	i = 0;
+	while (i < sentence.length())
+		putchar(toupper(sentence[i++]));
+}
