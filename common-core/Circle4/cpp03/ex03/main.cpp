0c0383996a06a0d4ebe93f0d45e6b9bcaf15258b
cde-sous <cde-sous@student.42.fr>
2025-03-01 12:36:32 +0100

    Create more tests to see if the values are correctly given when creating a copy

--- common-core/Circle4/cpp03/ex03/main.cpp
+++ common-core/Circle4/cpp03/ex03/main.cpp
@@ -5,61 +5,125 @@
 
 int main()
 {
-    ClapTrap    clap("clap1");  // take damage until dead
-    ClapTrap    clap2("clap2"); //  attack X even when X is dead + repair until no energy point
-    ClapTrap    clap3("clap3"); // attack until no energy point
-    ScavTrap    scav("scav");   // see attack difference in child + guard mode
-    ClapTrap    clap4("clap4"); // see attack difference in parent + guard mode
-    FragTrap    frag("frag");   // see highFiveGuys method
-    DiamondTrap diamond("diamond");
+    {
+        ClapTrap    clap("clap1");  // take damage until dead
+        ClapTrap    clap2("clap2"); //  attack X even when X is dead + repair until no energy point
+        ClapTrap    clap3("clap3"); // attack until no energy point
+        ScavTrap    scav("scav");   // see attack difference in child + guard mode
+        ClapTrap    clap4("clap4"); // see attack difference in parent + guard mode
+        FragTrap    frag("frag");   // see highFiveGuys method
+        DiamondTrap diamond("diamond");
 
-    clap.attack("clap2");
-    clap3.attack("clap2");
-    clap3.attack("clap2");
-    clap3.attack("clap2");
-    clap3.attack("clap2");
-    clap3.attack("clap2");
-    clap3.attack("clap2");
-    clap3.attack("clap2");
-    clap3.attack("clap2");
-    clap3.attack("clap2");
-    clap3.attack("clap2");
-    clap3.attack("clap2");
-    clap2.takeDamage(2);
-    clap2.takeDamage(2);
-    clap2.takeDamage(2);
-    clap2.takeDamage(2);
-    clap2.beRepaired(4);
-    clap2.beRepaired(7);
-    clap2.attack("clap");
-    clap.takeDamage(3);
-    clap.beRepaired(1);
-    clap.takeDamage(3);
-    clap.takeDamage(3);
-    clap.takeDamage(3);
-    clap.takeDamage(3);
-    clap.beRepaired(1);
-    clap.attack("clap2");
-    clap2.beRepaired(7);
-    clap2.beRepaired(7);
-    clap2.beRepaired(7);
-    clap2.beRepaired(7);
-    clap2.beRepaired(7);
-    clap2.beRepaired(7);
-    clap2.beRepaired(7);
-    clap2.beRepaired(7);
-    scav.attack("clap2");
-    scav.guardGate();
-    scav.guardGate();
-    scav.takeDamage(100);
-    scav.guardGate();
-    clap4.attack("scav");
-    frag.highFiveGuys();
-    frag.takeDamage(102);
-    frag.highFiveGuys();
-    diamond.attack("frag");
-    diamond.takeDamage(3);
-    for (int i = 0; i < 100; ++i)
+        clap.attack("clap2");
+        clap3.attack("clap2");
+        clap3.attack("clap2");
+        clap3.attack("clap2");
+        clap3.attack("clap2");
+        clap3.attack("clap2");
+        clap3.attack("clap2");
+        clap3.attack("clap2");
+        clap3.attack("clap2");
+        clap3.attack("clap2");
+        clap3.attack("clap2");
+        clap3.attack("clap2");
+        clap2.takeDamage(2);
+        clap2.takeDamage(2);
+        clap2.takeDamage(2);
+        clap2.takeDamage(2);
+        clap2.beRepaired(4);
+        clap2.beRepaired(7);
+        clap2.attack("clap");
+        clap.takeDamage(3);
+        clap.beRepaired(1);
+        clap.takeDamage(3);
+        clap.takeDamage(3);
+        clap.takeDamage(3);
+        clap.takeDamage(3);
+        clap.beRepaired(1);
+        clap.attack("clap2");
+        clap2.beRepaired(7);
+        clap2.beRepaired(7);
+        clap2.beRepaired(7);
+        clap2.beRepaired(7);
+        clap2.beRepaired(7);
+        clap2.beRepaired(7);
+        clap2.beRepaired(7);
+        clap2.beRepaired(7);
+        scav.attack("clap2");
+        scav.guardGate();
+        scav.guardGate();
+        scav.takeDamage(100);
+        scav.guardGate();
+        clap4.attack("scav");
+        frag.highFiveGuys();
+        frag.takeDamage(102);
+        frag.highFiveGuys();
         diamond.attack("frag");
-    diamond.whoAmI();
+        diamond.takeDamage(3);
+        for (int i = 0; i < 100; ++i)
+            diamond.attack("frag");
+        diamond.whoAmI();
+    }
+    std::cout << std::endl;
+    std::cout << "------------------------" << std::endl;
+    std::cout << std::endl;
+    {
+        DiamondTrap diamond;
+        DiamondTrap diamond2(diamond);
+        diamond2.attack("diamond"); // take 1 energy
+        diamond2.takeDamage(5);
+        diamond2.beRepaired(2); // take 1 energy
+        diamond2.guardGate();
+        diamond2.highFiveGuys();
+        diamond2.whoAmI();
+        diamond2.attack("diamond"); // all take 49 energy
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond");
+        diamond2.attack("diamond"); // no more energy because it takes energyPoints from Scav
+    }
 }
