c839c8be9aa142ba4b7022d390e244ef19f0c3e9
Christella De Sousa <cde-sous@paul-f3Br1s1.clusters.42paris.fr>
2025-03-20 14:24:16 +0100

    Add more examples

--- common-core/Circle5/cpp07/ex01/iter.hpp
+++ common-core/Circle5/cpp07/ex01/iter.hpp
@@ -2,6 +2,7 @@
 #define _ITER_HPP_
 
 #include <cstdlib>
+#include <iostream>
 
 template <typename T>
 T min(T a, T b)
@@ -9,11 +10,47 @@ T min(T a, T b)
 	return (a < b) ? a : b;
 }
 
+template <typename T>
+void swap(T& a, T& b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+// test for each type beside void
+template <typename T, size_t N>
+void iter(T (&array)[N], size_t size, T (*function)(T&))
+{
+	for (size_t i = 0; i < ::min(N, size); ++i)
+		function(array[i]);
+}
+
+// test for void
 template <typename T, size_t N>
 void iter(T (&array)[N], size_t size, void (*function)(T))
 {
 	for (size_t i = 0; i < ::min(N, size); ++i)
 		function(array[i]);
+}
+
+// test for const
+template <typename T, size_t N>
+void iter(T (&array)[N], size_t size, void (*function)(const T&))
+{
+	for (size_t i = 0; i < ::min(N, size); ++i)
+		function(array[i]);
+}
+
+// test for template1 having template2 as 3rd param
+template <typename T, size_t N>
+void iter(T (&array)[N], size_t size, void (*function)(T&, T&))
+{
+	for (size_t i = 0; i < ::min(N - 1, size - 1); ++i)
+	{
+		std::cout << "Comparing " << array[i] << " and " << array[i + 1] << std::endl;
+		function(array[i], array[i + 1]);
+	}
 }
 
 #endif
--- common-core/Circle5/cpp07/ex01/main.cpp
+++ common-core/Circle5/cpp07/ex01/main.cpp
@@ -16,12 +16,29 @@ void print(std::string str)
 	std::cout << str << std::endl;
 }
 
+// void print(const int& number)
+// {
+// 	std::cout << number << std::endl;
+// }
+
+int square(int& n)
+{
+	return n * n;
+}
+
 int main()
 {
 	int array1[] = { 3, 4, 8, 11, 21, 2, 5 };
 	char array2[] = { 'h', 'e', 'l', 'l', 'o' };
 	std::string array3[] = { "hello", "no", "goodbye" };
-	::iter(array1, 7, print);
+	int array4[] = { 1, 2, 4, 3 };
+
+	::iter(array1, 7, square);
+	for (int i = 0; array1[i]; ++i)
+		std::cout << array1[i] << std::endl;
 	::iter(array2, 7, print);
 	::iter(array3, 7, print);
+	::iter(array4, 7, swap);
+	for (int i = 0; array4[i]; ++i)
+		std::cout << array4[i] << std::endl;
 }
