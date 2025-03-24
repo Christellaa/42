852e96ffb47a88aa629fe88b5be7c63085a7e481
cde-sous <cde-sous@student.42.fr>
2025-03-24 21:38:22 +0100

    Norm cpp07, fix uninitialized values in main files

--- common-core/Circle5/cpp07/ex01/iter.hpp
+++ common-core/Circle5/cpp07/ex01/iter.hpp
@@ -4,53 +4,47 @@
 #include <cstdlib>
 #include <iostream>
 
-template <typename T>
-T min(T a, T b)
+template <typename T> T min(T a, T b)
 {
-	return (a < b) ? a : b;
+    return (a < b) ? a : b;
 }
 
-template <typename T>
-void swap(T& a, T& b)
+template <typename T> void swap(T& a, T& b)
 {
-	T tmp = a;
-	a = b;
-	b = tmp;
+    T tmp = a;
+    a     = b;
+    b     = tmp;
 }
 
 // test for each type beside void
-template <typename T, size_t N>
-void iter(T (&array)[N], size_t size, T (*function)(T&))
+template <typename T, size_t N> void iter(T (&array)[N], size_t size, T (*function)(T&))
 {
-	for (size_t i = 0; i < ::min(N, size); ++i)
-		function(array[i]);
+    for (size_t i = 0; i < ::min(N, size); ++i)
+        function(array[i]);
 }
 
 // test for void
-template <typename T, size_t N>
-void iter(T (&array)[N], size_t size, void (*function)(T))
+template <typename T, size_t N> void iter(T (&array)[N], size_t size, void (*function)(T))
 {
-	for (size_t i = 0; i < ::min(N, size); ++i)
-		function(array[i]);
+    for (size_t i = 0; i < ::min(N, size); ++i)
+        function(array[i]);
 }
 
 // test for const
-template <typename T, size_t N>
-void iter(T (&array)[N], size_t size, void (*function)(const T&))
+template <typename T, size_t N> void iter(T (&array)[N], size_t size, void (*function)(const T&))
 {
-	for (size_t i = 0; i < ::min(N, size); ++i)
-		function(array[i]);
+    for (size_t i = 0; i < ::min(N, size); ++i)
+        function(array[i]);
 }
 
 // test for template1 having template2 as 3rd param
-template <typename T, size_t N>
-void iter(T (&array)[N], size_t size, void (*function)(T&, T&))
+template <typename T, size_t N> void iter(T (&array)[N], size_t size, void (*function)(T&, T&))
 {
-	for (size_t i = 0; i < ::min(N - 1, size - 1); ++i)
-	{
-		std::cout << "Comparing " << array[i] << " and " << array[i + 1] << std::endl;
-		function(array[i], array[i + 1]);
-	}
+    for (size_t i = 0; i < ::min(N - 1, size - 1); ++i)
+    {
+        std::cout << "Comparing " << array[i] << " and " << array[i + 1] << std::endl;
+        function(array[i], array[i + 1]);
+    }
 }
 
 #endif
--- common-core/Circle5/cpp07/ex01/main.cpp
+++ common-core/Circle5/cpp07/ex01/main.cpp
@@ -3,17 +3,17 @@
 
 void print(char c)
 {
-	std::cout << c << std::endl;
+    std::cout << c << std::endl;
 }
 
 void print(int number)
 {
-	std::cout << number << std::endl;
+    std::cout << number << std::endl;
 }
 
 void print(std::string str)
 {
-	std::cout << str << std::endl;
+    std::cout << str << std::endl;
 }
 
 // void print(const int& number)
@@ -23,22 +23,22 @@ void print(std::string str)
 
 int square(int& n)
 {
-	return n * n;
+    return n *= n;
 }
 
 int main()
 {
-	int array1[] = { 3, 4, 8, 11, 21, 2, 5 };
-	char array2[] = { 'h', 'e', 'l', 'l', 'o' };
-	std::string array3[] = { "hello", "no", "goodbye" };
-	int array4[] = { 1, 2, 4, 3 };
+    int         array1[] = {3, 4, 8, 11, 21, 2, 5};
+    char        array2[] = {'h', 'e', 'l', 'l', 'o'};
+    std::string array3[] = {"hello", "no", "goodbye"};
+    int         array4[] = {1, 2, 4, 3};
 
-	::iter(array1, 7, square);
-	for (int i = 0; array1[i]; ++i)
-		std::cout << array1[i] << std::endl;
-	::iter(array2, 7, print);
-	::iter(array3, 7, print);
-	::iter(array4, 7, swap);
-	for (int i = 0; array4[i]; ++i)
-		std::cout << array4[i] << std::endl;
+    ::iter(array1, 7, square);
+    for (size_t i = 0; i < (sizeof(array1) / sizeof(array1[0])); ++i)
+        std::cout << array1[i] << std::endl;
+    ::iter(array2, 7, print);
+    ::iter(array3, 7, print);
+    ::iter(array4, 7, swap);
+    for (size_t i = 0; i < (sizeof(array4) / sizeof(array4[0])); ++i)
+        std::cout << array4[i] << std::endl;
 }
